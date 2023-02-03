d1d14cd829e666564fac07f4bb95ff71c7836618
AyushiMalviya08 <115863792+AyushiMalviya08@users.noreply.github.com>
2023-02-03 21:19:26 +0530

    Add files via upload

---
+++ src/component/header/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navbar.css";
+
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div clasName ="left">
+                <div className="navlogo">
+                    <img src="./logoo.png" alt="" />
+                </div>
+                <div className="nav_searchbar">
+                    <input type="text" name=" " id=" " />
+                    
+                
+                    
+                </div>
+        
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
